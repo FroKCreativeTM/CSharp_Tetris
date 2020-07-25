f5168c2ce1ed320046cacfb54e1d3ddb3ac1b36c
seungCheolCha <cscculture95@gmail.com>
2020-07-26 00:53:58 +0900

    블록 타입과 데이터를 생성

--- CSharp_Tetris/Block.cs
+++ CSharp_Tetris/Block.cs
@@ -4,48 +4,63 @@ using System.Text;
 
 namespace CSharp_Tetris
 {
+    // 블록의 방향 enum입니다.
+    enum BLOCK_DIRECTION
+    {
+        BD_LEFT,
+        BD_TOP,
+        BD_RIGHT,
+        BD_BOTTOM,
+
+        BD_MAX          // Iteration용
+    }
+
+    // 블록의 타입 enum입니다.
     enum BLOCKTYPE
     {
         BT_I,   // I 형태 블록
         BT_L,   // L 형태 블록
         BT_J,   // J 형태 블록
         BT_Z,   // Z 형태 블록
-        BT_S,   // S 형태 블록
+        BT_S,   // Stick 형태 블록
         BT_T,   // T 형태 블록
-        BT_O    // 네모 형태 블록
+        BT_O,    // 네모 형태 블록
+
+
+        BT_MAX    // Iteration용
     }
 
 
-    class Block
+    partial class Block
     {
-        int x = 0;
-        int y = 0;
+        // 블록의 위치
+        int m_x = 0;
+        int m_y = 0;
 
-        // 4x4의 저장 공간이 필요하다.
-        List<List<string>> BlockData = new List<List<string>>();
-        BLOCKTYPE blockType;
+        // 이 블록의 타입과 방향이다.
+        string[][] blockShape = null;
+        // BLOCK_DIRECTION blockDirection = BLOCK_DIRECTION.BD_TOP;
+
+        // 이 블록이 렌더링될 스크린의 정보이다.
         GameScreen screenInfo = null;
 
         // 게임 스크린을 지정한다.
         public Block(GameScreen _screen)
         {
+            // 스크린 정보를 갱신한다.
             screenInfo = _screen;
 
-            for (int y = 0; y < 4; y++)
-            {
-                BlockData.Add(new List<string>());
-            }
+            // 블록 데이터를 초기화한다.
+            DataInit();
 
+            // 일단
+            SettingBlock(BLOCKTYPE.BT_T, BLOCK_DIRECTION.BD_RIGHT);
+        }
 
-
-            // 4x4안에 뭐든 들어간다.
-            for (int y = 0; y < 4; y++)
-            {
-                for (int x = 0; x < 4; x++)
-                {
-
-                }
-            }
+        // 블록 모양을 결정한다.
+        private void SettingBlock(BLOCKTYPE _eBlockType, BLOCK_DIRECTION _eBlockDirection)
+        {
+            blockShape = AllBlock[(int)_eBlockType][(int)_eBlockDirection];
         }
 
         // 입력을 처리한다.
@@ -63,14 +78,14 @@ namespace CSharp_Tetris
             switch (Console.ReadKey().Key)
             {
             case ConsoleKey.A:
-                x--;
+                m_x--;
                 break;
             case ConsoleKey.D:
-                x++;
+                m_x++;
                 break;
             // 한 번에 떨어뜨리는 키
             case ConsoleKey.S:
-                y++;
+                m_y++;
                 break;
             default:
                 break;
@@ -80,11 +95,24 @@ namespace CSharp_Tetris
         // 블록의 움직임을 처리한다.
         public void Move()
         {
+            // 입력 처리를 한다.
             Input();
-            screenInfo.SetBlock(y, x, "■");
-            screenInfo.SetBlock(y + 1, x, "■");
-            screenInfo.SetBlock(y + 2, x, "■");
-            screenInfo.SetBlock(y + 3, x, "■");
+
+            // 블럭의 모양대로 그리도록 스크린에 넘겨준다..
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    // 만약 공백이라면 처음부터 그리지 않는다.
+                    if(blockShape[y][x] == "□")
+                    {
+                        continue;
+                    }
+                        
+                    // 나머지는 그릴 수 있게 블록을 설정한다.
+                    screenInfo.SetBlock(m_y + y, m_x + x, blockShape[y][x]);
+                }
+            }
         }
     }
 }
---
+++ CSharp_Tetris/BlockData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tetris
+{
+    // 블록 클래스의 데이터를 다루는 부분입니다.
+    partial class Block
+    {
+        // 모든 블록의 정보이다.
+        // 이것이 있는 이유는 단순하다.
+        // 어떤 블록이 생성될 때는 이 블록 정보를 참조해서 
+        // 블록의 타입과 방향을 참조하며, 이를 통해서 블록의 모양을 어떻게 스크린에 그릴 것인가를 알 수 있다.
+        List<List<string[][]>> AllBlock = new List<List<string[][]>>();
+
+        // 블록 데이터를 초기화합니다.
+        void DataInit()
+        {
+            for (int BT = 0; BT < (int)BLOCKTYPE.BT_MAX; BT++)
+            {
+                AllBlock.Add(new List<string[][]>());
+
+                for (int BD = 0; BD < (int)BLOCK_DIRECTION.BD_MAX; BD++)
+                {
+                    AllBlock[BT].Add(null);
+                }
+            }
+
+            #region S
+            // Stick 모양입니다.
+            // Stick타입 블록의 TOP
+            AllBlock[(int)BLOCKTYPE.BT_S][(int)BLOCK_DIRECTION.BD_TOP] = new string[][]
+            {
+                new string[]{ "■", "□", "□", "□" },
+                new string[]{ "■", "□", "□", "□" },
+                new string[]{ "■", "□", "□", "□" },
+                new string[]{ "■", "□", "□", "□" }
+            };
+
+            // Stick타입 블록의 RIGHT
+            AllBlock[(int)BLOCKTYPE.BT_S][(int)BLOCK_DIRECTION.BD_RIGHT] = new string[][]
+            {
+                new string[]{ "■", "■", "■", "■" },
+                new string[]{ "□", "□", "□", "□" },
+                new string[]{ "□", "□", "□", "□" },
+                new string[]{ "□", "□", "□", "□" }
+            };
+
+            // Stick타입 블록의 BOTTOM
+            AllBlock[(int)BLOCKTYPE.BT_S][(int)BLOCK_DIRECTION.BD_BOTTOM] = new string[][]
+            {
+                new string[]{ "■", "□", "□", "□" },
+                new string[]{ "■", "□", "□", "□" },
+                new string[]{ "■", "□", "□", "□" },
+                new string[]{ "■", "□", "□", "□" }
+            };
+
+            // Stick타입 블록의 LEFT
+            AllBlock[(int)BLOCKTYPE.BT_S][(int)BLOCK_DIRECTION.BD_LEFT] = new string[][]
+            {
+                new string[]{ "■", "■", "■", "■" },
+                new string[]{ "□", "□", "□", "□" },
+                new string[]{ "□", "□", "□", "□" },
+                new string[]{ "□", "□", "□", "□" }
+            };
+            #endregion
+            #region T
+            // T 모양입니다.
+            // I타입 블록의 TOP
+            AllBlock[(int)BLOCKTYPE.BT_T][(int)BLOCK_DIRECTION.BD_TOP] = new string[][]
+            {
+                new string[]{ "□", "■", "□", "□" },
+                new string[]{ "■", "■", "■", "□" },
+                new string[]{ "□", "□", "□", "□" },
+                new string[]{ "□", "□", "□", "□" }
+            };
+
+            // T타입 블록의 RIGHT
+            AllBlock[(int)BLOCKTYPE.BT_T][(int)BLOCK_DIRECTION.BD_RIGHT] = new string[][]
+            {
+                new string[]{ "□", "■", "□", "□" },
+                new string[]{ "□", "■", "■", "□" },
+                new string[]{ "□", "■", "□", "□" },
+                new string[]{ "□", "□", "□", "□" }
+            };
+
+            // T타입 블록의 BOTTOM
+            AllBlock[(int)BLOCKTYPE.BT_T][(int)BLOCK_DIRECTION.BD_BOTTOM] = new string[][]
+            {
+                new string[]{ "□", "□", "□", "□" },
+                new string[]{ "■", "■", "■", "□" },
+                new string[]{ "□", "■", "□", "□" },
+                new string[]{ "□", "□", "□", "□" }
+            };
+
+            // T타입 블록의 LEFT
+            AllBlock[(int)BLOCKTYPE.BT_T][(int)BLOCK_DIRECTION.BD_LEFT] = new string[][]
+            {
+                new string[]{ "□", "■", "□", "□" },
+                new string[]{ "■", "■", "□", "□" },
+                new string[]{ "□", "■", "□", "□" },
+                new string[]{ "□", "□", "□", "□" }
+            };
+            #endregion
+        }
+    }
+}
